813a21fef17e242751297acd73cd20b4beee766e
Jusun Park <jusun.park@mytrum.com>
2024-11-05 12:36:06 +0900

    Calculate Create

---
+++ src/Calculate.kt
@@ -0,0 +1,21 @@
+import java.util.*
+
+fun sum(a: Int, b: Int): Int = a + b
+fun subtraction(a: Int, b: Int): Int = a - b
+fun multiply(a: Int, b: Int): Int = a * b
+fun divide(a: Int, b: Int): Int = a / b
+
+fun main() {
+    val sc: Scanner = Scanner(System.`in`)
+
+    print("Please enter the first number : ")
+    val firstNum: Int = sc.nextInt()
+
+    print("Please enter the second number : ")
+    val secondNum: Int = sc.nextInt()
+
+    println("sum : $firstNum + $secondNum = " + sum(firstNum, secondNum))
+    println("subtraction : $firstNum - $secondNum = " + subtraction(firstNum, secondNum))
+    println("multiply : $firstNum * $secondNum = " + multiply(firstNum, secondNum))
+    println("divide : $firstNum / $secondNum = " + divide(firstNum, secondNum))
+}
